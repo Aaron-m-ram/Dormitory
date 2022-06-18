8a3987ea1bafadb7eb0be94cfe439e96a57b16ce
Aaron Ramirez <amramirez1222@gmail.com>
2022-06-17 17:08:31 -1000

    clean up

--- Dormitory/Dormitory/Nav/Home.swift
+++ Dormitory/Dormitory/Nav/Home.swift
@@ -9,62 +9,63 @@ import SwiftUI
 
 struct Home: View {
     var body: some View {
-        VStack{
-            Text("JBPHH Dorms")
-                .font(.title)
-                .bold()
-            VStack{
-                Image(systemName: "wrench.and.screwdriver")
-                    .resizable()
-                    .frame(width: 75, height: 75)
-                    //.padding()
-                Text("Maintenance")
-                    .font(.title)
-                Text("report a leaky faucet, broken locks, etc.")
-                    .font(.subheadline)
-            }
-            .frame(width: 350, height: 150)
-            .overlay(
-                RoundedRectangle(cornerRadius: 5)
-                    .stroke(.black))
-            .padding(.bottom)
-            
-                VStack{
-                    Image(systemName: "hand.wave")
-                        .resizable()
-                        .frame(width: 75, height: 75)
-                        //.padding()
-                    Text("Help")
-                        .font(.title)
-                    Text("Locked out or need helped after hours")
-                        .font(.subheadline)
-                }
-                .frame(width: 350, height: 150)
-                .overlay(
-                    RoundedRectangle(cornerRadius: 5)
-                        .stroke(.black))
-                .padding(.bottom)
-                
-            VStack{
-                Image(systemName: "exclamationmark.3")
-                    .resizable()
-                    .frame(width: 75, height: 75)
-                    //.padding()
-                Text("Help")
-                    .font(.title)
-                Text("Locked out or need helped after hours")
-                    .font(.subheadline)
-            }
-            .frame(width: 350, height: 150)
-            .overlay(
-                RoundedRectangle(cornerRadius: 5)
-                    .stroke(.black))
-            .padding(.bottom)
-            
-        }
-        
+        NavigationView {
+            VStack(spacing: 30) {
 
+                NavigationLink(destination: Services()) {
+                    //Text("Choose Heads")
+                    VStack{
+                        Image(systemName: "wrench.and.screwdriver")
+                            .resizable()
+                            .frame(width: 75, height: 75)
+                        Text("maintenance")
+                            .font(.title)
+                        Text("report a leaky faucet, broken locks, etc...")
+                            .font(.subheadline)
+                    }
+                    .frame(width: 350, height: 150)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 5)
+                            .stroke())
+                    
+                }
+
+                NavigationLink(destination: Services()) {
+                    //Text("Choose Tails")
+                    VStack{
+                        Image(systemName: "hand.wave")
+                            .resizable()
+                            .frame(width: 75, height: 75)
+                        Text("Help")
+                            .font(.title)
+                        Text("Locked out or need helped after hours")
+                            .font(.subheadline)
+                    }
+                    .frame(width: 350, height: 150)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 5)
+                            .stroke())
+                }
+                NavigationLink(destination: Emer()){
+                    //Text("Emer")
+                    VStack{
+                        Image(systemName: "exclamationmark.3")
+                            .resizable()
+                            .frame(width: 75, height: 75)
+                        Text("Emergency")
+                            .font(.title)
+                        Text("maintenance emergency or need to talk to management")
+                            .font(.subheadline)
+                    }
+                    .frame(width: 350, height: 150)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 5)
+                            .stroke())
+                }
+            }
+            .navigationTitle("Dormitory")
     }
+}
 }
 
 struct Home_Previews: PreviewProvider {
@@ -72,3 +73,69 @@ struct Home_Previews: PreviewProvider {
         Home()
     }
 }
+
+
+
+//struct Home: View {
+//    var body: some View {
+//        NavigationView
+//        VStack{
+//            Text("JBPHH Dorms")
+//                .font(.title)
+//                .bold()
+//            VStack{
+//                Image(systemName: "wrench.and.screwdriver")
+//                    .resizable()
+//                    .frame(width: 75, height: 75)
+//                    //.padding()
+//                Text("Maintenance")
+//                    .font(.title)
+//                Text("report a leaky faucet, broken locks, etc.")
+//                    .font(.subheadline)
+//            }
+//            .frame(width: 350, height: 150)
+//            .overlay(
+//                RoundedRectangle(cornerRadius: 5)
+//                    .stroke(.black))
+//            .padding(.bottom)
+//            //NavigationLink(placeholder){
+//            //    services()
+//            //}
+//
+//                VStack{
+//                    Image(systemName: "hand.wave")
+//                        .resizable()
+//                        .frame(width: 75, height: 75)
+//                        //.padding()
+//                    Text("Help")
+//                        .font(.title)
+//                    Text("Locked out or need helped after hours")
+//                        .font(.subheadline)
+//                }
+//                .frame(width: 350, height: 150)
+//                .overlay(
+//                    RoundedRectangle(cornerRadius: 5)
+//                        .stroke(.black))
+//                .padding(.bottom)
+//
+//            VStack{
+//                Image(systemName: "exclamationmark.3")
+//                    .resizable()
+//                    .frame(width: 75, height: 75)
+//                    //.padding()
+//                Text("Help")
+//                    .font(.title)
+//                Text("Locked out or need helped after hours")
+//                    .font(.subheadline)
+//            }
+//            .frame(width: 350, height: 150)
+//            .overlay(
+//                RoundedRectangle(cornerRadius: 5)
+//                    .stroke(.black))
+//            .padding(.bottom)
+//
+//        }
+//
+//
+//    }
+//}
