44ef3b46d9d0207feb63527d7685b1be15bd4baa
Aaron Ramirez <amramirez1222@gmail.com>
2022-06-17 17:35:48 -1000

    Service page fixed w/ bug
    service page goes to correct area, problem nav pages stacking on top of each other

---
+++ Dormitory/Dormitory/mainxPath/AirForceDorms.swift
@@ -0,0 +1,22 @@
+//
+//  AirForceDorm.swift
+//  Dormitory
+//
+//  Created by Aaron Ramirez on 6/17/22.
+//
+
+import SwiftUI
+
+struct AirForceDorms: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            .bold()
+            .background(.brown)
+    }
+}
+
+struct AirForceDorm_Previews: PreviewProvider {
+    static var previews: some View {
+        AirForceDorms()
+    }
+}
